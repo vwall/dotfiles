a7a1b304a4164a594a5aa72eff75b9efbb2898c7
Vincent Waller <vrwaller@gmail.com>
2017-06-08 09:43:03 -0700

    Move fish

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,7 @@ set --global --export PATH \
 for dircolors in {,g}dircolors
   type -fp $dircolors >/dev/null 2>&1; and source (eval "$dircolors --c-shell ~/.dircolorsrc|psub")
 end
+
+# Chruby
+source /usr/local/share/chruby/chruby.fish
+source /usr/local/share/chruby/auto.fish
