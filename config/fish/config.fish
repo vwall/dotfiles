21f47d8a896933ab56e632417025e0210e9d2b80
Vincent Waller <vrwaller@gmail.com>
2017-06-07 23:21:45 -0700

    Add paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,10 @@
+set --global --export PATH \
+  $HOME/bin \
+  /usr/local/{,s}bin \
+  /usr/{,s}bin \
+  /{,s}bin
+
+
 for dircolors in {,g}dircolors
   type -fp $dircolors >/dev/null 2>&1; and source (eval "$dircolors --c-shell ~/.dircolorsrc|psub")
 end
