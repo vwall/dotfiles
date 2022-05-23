579c8f75b098dc678ce2e6981a7bff85ca6016c1
Vincent Waller <vrwaller@gmail.com>
2022-05-22 17:22:26 -0700

    add homebrew to path

--- configs/config/fish/config.fish
+++ configs/config/fish/config.fish
@@ -1,6 +1,7 @@
 # Fish config
 set -gx BORK_COLOR true
 
+set PATH /opt/homebrew/bin $PATH
 set PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
 
 source (brew --prefix asdf)/asdf.fish
